c77c726c23f18bbff3ce2974a1ba094232caaf74
Yuvaraj-0 <yuvaraj525354@gmail.com>
2025-07-24 20:21:47 +0530

    Update axiosClient.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3002', // your API base URL
+  baseURL: 'https://your-backend-url.onrender.com/api/stats', // your API base URL
 });
 
 // Add token to every request if present
